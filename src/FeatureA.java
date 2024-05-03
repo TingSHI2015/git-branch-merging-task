bcb5c5f7c4a133e9d7073be4090da1aeecc23ff9
tingSHI <ting.shi2015@gmail.com>
2024-05-03 16:37:28 +0200

    add name ting2 in FeatureA

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -4,5 +4,7 @@ public class FeatureA{
 
     public static void main(String[] args) {
         System.out.println("Hallo, das ist der Code von <Ting>");  //Step1
+        System.out.println("Hallo, das ist der Code von <Ting2 !>");
+
     }
 }
